2fa0e214d578b46535e923b8a667f2168ce74483
ariel <ariel.wm1@gmail.com>
2023-07-01 13:34:26 -0400

    fix edit user

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,8 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types'
-import { IsString } from 'class-validator'
+import { IsBoolean, IsDate, IsOptional, IsString } from 'class-validator'
 import { CreateUserDto } from './create-user.dto'
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsString()
   id: string
+
+  @IsBoolean()
+  isActive: boolean
+
+  @IsString()
+  @IsOptional()
+  photoURL?: string
+
+  @IsDate()
+  @IsOptional()
+  createdAt?: Date
+
+  @IsDate()
+  @IsOptional()
+  updatedAt?: Date
 }
