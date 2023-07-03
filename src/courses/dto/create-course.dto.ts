319f7ee842b557842b16e895f1171313b7ffe40b
ariel <ariel.wm1@gmail.com>
2023-07-02 20:57:45 -0400

    course dto & entity

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCourseDto {}
+import { IsArray, IsString } from 'class-validator'
+
+export class CreateCourseDto {
+  @IsString()
+  code: string
+
+  @IsString()
+  teacher: string
+
+  @IsString({ each: true })
+  @IsArray()
+  students: string[]
+}
--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,1 +1,19 @@
-export class Course {}
+import { User } from 'src/auth/entities/user.entity'
+import { Subject } from '../../subjects/entities/subject.entity'
+import { Entity, ManyToOne, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm'
+
+@Entity('course')
+export class Course {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @ManyToOne(() => User)
+  teacher: User
+
+  @ManyToOne(() => Subject)
+  subject: Subject
+
+  @ManyToMany(() => User)
+  @JoinTable()
+  students: User[]
+}
